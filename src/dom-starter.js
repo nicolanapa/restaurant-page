2a8c1b9ab3efad6ffea1a186a4ccb6faf1327610
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-01-11 19:38:59 +0100

    Worked on main page style

--- src/dom-starter.js
+++ src/dom-starter.js
@@ -8,6 +8,8 @@ import immagine from "./restaurant-image.jpg";
 
 function startPage() {
     const content = document.querySelector("#content");
+    const mainPage = document.createElement("div");
+    mainPage.setAttribute("id", "mainPage");
 
     let title = document.createElement("h1");
     title.innerText = "The Finest Restaurant";
@@ -18,12 +20,14 @@ function startPage() {
     image.classList.add("restaurantFoto");
 
     let restaurantDescription = document.createElement("p");
-    restaurantDescription.innerText = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae adipisci inventore veniam necessitatibus fugiat, eos eligendi a officiis non magni molestiae incidunt rem illo, sapiente maiores dolores neque possimus exercitationem. At, ea sit rem commodi, repellendus corporis omnis maxime dignissimos repellat sed laborum quis ipsam ipsa ut vel pariatur est quidem harum adipisci accusamus voluptas? Obcaecati ea voluptate veritatis et?";
+    restaurantDescription.innerText = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae adipisci inventore veniam necessitatibus fugiat, eos eligendi a officiis non magni molestiae incidunt rem illo, sapiente maiores dolores neque possimus exercitationem. At, ea sit rem commodi, repellendus corporis omnis maxime dignissimos repellat sed laborum quis ipsam ipsa ut vel pariatur est quidem harum adipisci accusamus voluptas?";
     restaurantDescription.classList.add("restaurantDescription");
 
-    content.appendChild(title);
-    content.appendChild(image);
-    content.appendChild(restaurantDescription);
+    content.appendChild(mainPage);
+
+    mainPage.appendChild(title);
+    mainPage.appendChild(image);
+    mainPage.appendChild(restaurantDescription);
 }
 
 export default startPage;
